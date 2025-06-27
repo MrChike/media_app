9533bb35924e16424c8b72fbc8dda9a57fa5cc00
MrChike <chikeegonu@gmail.com>
2025-06-27 21:48:52 +0100

    Update series_b_setup.sh

--- series_b_setup.sh
+++ series_b_setup.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-touch celeryconfig.py docker-compose.yaml Dockerfile Dockerfile.nginx Dockerfile.redis nginx.conf redis.conf entrypoint.sh
+touch celeryconfig.py docker-compose.api.yaml docker-compose.db.yaml Dockerfile Dockerfile.nginx nginx.conf entrypoint.sh
 
+printf "# Note:\n# Containers communicate using Docker Compose service names, ensuring reliable \n# networking without the need to manage specific IP addresses.\n\n# OMDb API Configuration\nOMDB_MOVIES_API_KEY=YOUR_API_KEY_HERE # Sign up at https://www.omdbapi.com/apikey.aspx\n\n# PostgreSQL Configuration\nPOSTGRES_USER=root\nPOSTGRES_PASSWORD=root\nPOSTGRES_HOST=localhost\nPOSTGRES_PORT=5432\nPOSTGRES_NAME=root\n\n# MongoDB Configuration\nMONGODB_USER=root\nMONGODB_PASSWORD=root\nMONGODB_HOST=localhost\nMONGODB_PORT=27017\nMONGODB_NAME=root\n\n# Redis Configuration\nREDIS_PASSWORD=root\nREDIS_HOST=localhost\nREDIS_PORT=6379\n" > .example.env
+
+cp .example.env .env
 rm shared/utils/fetch_request_with_error_handlling.py
 mv setup.sh series_a_setup.sh
+mv shared/config/settings.py shared/config/base_settings.py
 
-touch shared/config/base_settings.py \
+touch shared/config/settings.py \
 shared/db/connection.py \
 shared/utils/fetch_request_with_error_handling.py \
 tests/movies/test_tasks.py
+
 
 python3 -m venv env 
 source env/bin/activate

84ef36c9929ff2c91a8c2755890f26e59941eb62
MrChike <chikeegonu@gmail.com>
2025-06-28 11:58:20 +0100

    Update series_b_setup.sh

--- series_b_setup.sh
+++ series_b_setup.sh
@@ -2,7 +2,7 @@
 
 touch celeryconfig.py docker-compose.api.yaml docker-compose.db.yaml Dockerfile Dockerfile.nginx nginx.conf entrypoint.sh
 
-printf "# Note:\n# Containers communicate using Docker Compose service names, ensuring reliable \n# networking without the need to manage specific IP addresses.\n\n# OMDb API Configuration\n# Sign up at https://www.omdbapi.com/apikey.aspx\nOMDB_MOVIES_API_KEY=YOUR_API_KEY_HERE \n# PostgreSQL Configuration\nPOSTGRES_USER=root\nPOSTGRES_PASSWORD=root\nPOSTGRES_HOST=localhost\nPOSTGRES_PORT=5432\nPOSTGRES_NAME=root\n\n# MongoDB Configuration\nMONGODB_USER=root\nMONGODB_PASSWORD=root\nMONGODB_HOST=localhost\nMONGODB_PORT=27017\nMONGODB_NAME=root\n\n# Redis Configuration\nREDIS_PASSWORD=root\nREDIS_HOST=localhost\nREDIS_PORT=6379\n" > .example.env
+printf "# Note:\n# Containers communicate using Docker Compose service names, ensuring reliable \n# networking without the need to manage specific IP addresses.\n\n# OMDb API Configuration\n# Sign up at https://www.omdbapi.com/apikey.aspx\nOMDB_MOVIES_API_KEY=YOUR_API_KEY_HERE \n\n# PostgreSQL Configuration\nPOSTGRES_USER=root\nPOSTGRES_PASSWORD=root\nPOSTGRES_HOST=localhost\nPOSTGRES_PORT=5432\nPOSTGRES_NAME=root\n\n# MongoDB Configuration\nMONGODB_USER=root\nMONGODB_PASSWORD=root\nMONGODB_HOST=localhost\nMONGODB_PORT=27017\nMONGODB_NAME=root\n\n# Redis Configuration\nREDIS_PASSWORD=root\nREDIS_HOST=localhost\nREDIS_PORT=6379\n" > .example.env
 
 cp .example.env .env
 rm shared/utils/fetch_request_with_error_handlling.py

af7377e77cf1197106fe09b45277ffee23e27589
mrchike <chikeegonu@gmail.com>
2025-07-08 06:10:58 +1000

    crud operations

--- tests/movies/test_controller.py
+++ tests/movies/test_controller.py
@@ -1,72 +1,151 @@
-import unittest
-import asyncio
 import json
-from unittest.mock import MagicMock
+import unittest
+from unittest.mock import AsyncMock, MagicMock
+from fastapi.responses import JSONResponse
 from movies.controller import MovieController
 from movies.schema import MovieSchema
-from fastapi.responses import JSONResponse
-from movies.service import MovieService
 
 
-class TestMovieController(unittest.TestCase):
+class TestMovieController(unittest.IsolatedAsyncioTestCase):
 
     def setUp(self):
-        """Setup test environment before each test."""
-        # Create a mock for MovieService
-        self.mock_service = MagicMock(spec=MovieService)
-        
-        # Initialize MovieController with the mocked service
         self.controller = MovieController()
-        self.controller.service = self.mock_service
-        
-    def test_get_movie_success(self):
-        """Test successful movie retrieval."""
-        # Create a mock response from the service
-        mock_response = {
-            "status_code": 200,
-            "data": {"title": "Inception", "actors": "Leonardo DiCaprio, Joseph Gordon-Levitt"}
+        self.controller.service = MagicMock()
+        self.mock_request = MovieSchema(title="Inception")
+
+    async def test_get_movie(self):
+        expected_response_data = {
+            "title": "Inception",
+            "actors": "Leonardo DiCaprio",
+            "year": 2010
         }
-        
-        # Mock the service's get_movie_details method to return the mock response
-        self.mock_service.get_movie_details.return_value = mock_response
-        
-        # Create a test movie schema input (this simulates the input data)
-        test_movie = MovieSchema(title="Inception", actors="Leonardo DiCaprio")
-        
-        # Call the controller method (simulate POST request)
-        response = asyncio.run(self.controller.get_movie(test_movie))
-        
-        # Validate the response
+
+        self.controller.service.get_movie_details = AsyncMock(
+            return_value={"data": expected_response_data, "status_code": 200}
+        )
+
+        response = await self.controller.get_movie(self.mock_request)
+
         self.assertIsInstance(response, JSONResponse)
         self.assertEqual(response.status_code, 200)
-        import json
-        self.assertEqual(json.loads(response.body), {"data": mock_response["data"]})  # Check the response body
+        self.assertEqual(json.loads(response.body), {
+            "data": {
+                "title": "Inception",
+                "actors": "Leonardo DiCaprio",
+                "year": 2010
+            }
+        })
 
-
-    def test_get_movie_failure(self):
-        """Test movie retrieval failure scenario."""
-        # Create a mock failure response from the service
-        mock_response = {
-            "status_code": 503,
-            "data": "Sorry, we couldn't fetch the movie details right now. Please try again later."
+    async def test_create_movie_postgres(self):
+        expected_response_data = {
+            'data': 'Movie Created in Postgres DB Successfully'
         }
-        
-        # Mock the service's get_movie_details method to return the failure response
-        self.mock_service.get_movie_details.return_value = mock_response
-        
-        # Create a test movie schema input
-        test_movie = MovieSchema(title="Unknown Movie", actors="Unknown Actor")
 
-        # Call the controller method (simulate POST request)
-        response = asyncio.run(self.controller.get_movie(test_movie))
+        self.controller.service.create_movie_postgres = AsyncMock(
+            return_value={
+                "data": expected_response_data['data'],
+                "status_code": 201
+            }
+        )
 
-        # Validate the response
+        response = await self.controller.create_movie_postgres(
+            self.mock_request
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(json.loads(response.body), expected_response_data)
+
+    async def test_create_movie_mongo(self):
+        expected_response_data = {
+            'data': 'Movie Created in Mongo DB Successfully'
+        }
+
+        self.controller.service.create_movie_mongo = AsyncMock(
+            return_value={
+                "data": expected_response_data["data"],
+                "status_code": 201
+            }
+        )
+
+        response = await self.controller.create_movie_mongo(self.mock_request)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(json.loads(response.body), expected_response_data)
+
+    async def test_create_movie_external(self):
+        expected_response_data = {
+            'data': (
+                'Movie Created Successfully in both Postgres & MongoDB '
+                'from Redis DB Cache'
+            )
+        }
+
+        self.controller.service.create_movie_external_from_cache = AsyncMock(
+            return_value={
+                "data": expected_response_data['data'],
+                "status_code": 201
+            }
+        )
+
+        response = await self.controller.create_movie_external(
+            self.mock_request.title
+        )
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(json.loads(response.body), expected_response_data)
+
+    async def test_delete_movie_success(self):
+        expected_response_data = {
+            "data": "Movie Deleted Successfully Across all DB"
+        }
+
+        self.controller.service.delete_movie = AsyncMock(
+            return_value={
+                "data": expected_response_data["data"],
+                "status_code": 200
+            }
+        )
+
+        response = await self.controller.delete_movie("Inception")
+
         self.assertIsInstance(response, JSONResponse)
-        self.assertEqual(response.status_code, 503)
-        self.assertEqual(json.loads(response.body), {"data": mock_response["data"]})
-        self.assertEqual(json.loads(response.body), {"data": mock_response["data"]})
-        self.assertEqual(json.loads(response.body), {"data": mock_response["data"]})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.body), expected_response_data)
 
-# Run the tests
+    async def test_delete_movie_not_found(self):
+        expected_response_data = {
+            "data": "Movie not found in DB"
+        }
+
+        self.controller.service.delete_movie = AsyncMock(
+            return_value={
+                "data": expected_response_data["data"],
+                "status_code": 501
+            }
+        )
+
+        response = await self.controller.delete_movie("NonExistentMovie")
+
+        self.assertIsInstance(response, JSONResponse)
+        self.assertEqual(response.status_code, 501)
+        self.assertEqual(json.loads(response.body), expected_response_data)
+
+    # def test_process_cpu_bound_tasks(self):
+    #     expected_response_data = {
+    #         'data': (
+    #             'Successfully Processing Time & Resource Intense Task '
+    #             'in the background....'
+    #         )
+    #     }
+
+    #     self.controller.service.trigger_cpu_bound_task = MagicMock()
+
+    #     response = self.controller.process_cpu_bound_tasks()
+
+    #     self.controller.service.trigger_cpu_bound_task.assert_called_once()
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(json.loads(response.body), expected_response_data)
+
+
 if __name__ == "__main__":
     unittest.main()
